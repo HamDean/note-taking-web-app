a5948e5c94c6085eb5e26c674752b4e68806de1c
Hamdeen <hamdeen247@gmail.com>
2026-05-30 16:17:51 +0000

    Implement tab selection functionality

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,20 +1,31 @@
 import logo from "../assets/images/logo.svg";
 import Tab from "./Tab";
 import TagsSection from "./TagsSection";
-import { sidetabs } from '../utils/constansts'
+import { sidetabs } from "../utils/constansts";
+import { useState } from "react";
 
 const SideBar = () => {
+  const [selectedTab, setSelectedTab] = useState("All Notes");
   const tags = ["Cooking", "Finance"];
 
   return (
-    <article className="hidden fixed top-0 lg:flex w-68 flex-col gap-4 border-r border-neutral-200 h-screen px-4 py-3">
+    <article
+      className="hidden fixed top-0 lg:flex w-68 flex-col gap-4 
+                       border-r border-neutral-200 h-screen px-4 py-3"
+    >
       <div>
         <img src={logo} />
       </div>
 
       <div>
         {sidetabs.map((tab, index) => (
-          <Tab key={index} iconSrc={tab.src} tabTitle={tab.title} />
+          <Tab
+            key={index}
+            iconSrc={tab.src}
+            tabTitle={tab.title}
+            onSelectTab={(tab) => setSelectedTab(tab)}
+            isSelected={tab.title === selectedTab}
+          />
         ))}
       </div>
 
