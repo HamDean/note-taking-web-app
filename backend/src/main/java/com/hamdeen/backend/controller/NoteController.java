b1ed1fd74e17f7985ef3b1525be130326d900c34
Hamdeen <hamdeen247@gmail.com>
2026-05-31 09:46:24 +0000

    Set up note controller

---
+++ backend/src/main/java/com/hamdeen/backend/controller/NoteController.java
@@ -0,0 +1,10 @@
+package com.hamdeen.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notes")
+public class NoteController {
+    // TODO: implement notes endpoints
+}
