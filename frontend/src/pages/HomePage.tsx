0bacffab87d2976859a7bbe0e49c4bc56a745b74
Hamdeen <hamdeen247@gmail.com>
2026-05-31 11:10:14 +0000

    Divide desktop homepage into sections.

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -8,29 +8,43 @@ const HomePage = () => {
   const [selectedIndex, setSelectedIndex] = useState(-1);
   const { note = "All Notes" } = useParams();
   const notes = [
-    // { title: "title 1", tags: ["tag 1", "tag 2"], dateCreated: "29 May 2015" },
-    // { title: "title 1", tags: ["tag 1", "tag 2"], dateCreated: "29 May 2015" },
-    // { title: "title 1", tags: ["tag 1", "tag 2"], dateCreated: "29 May 2015" },
+    { title: "title 1", tags: ["tag 1", "tag 2"], dateCreated: "29 May 2015" },
+    { title: "title 1", tags: ["tag 1", "tag 2"], dateCreated: "29 May 2015" },
+    { title: "title 1", tags: ["tag 1", "tag 2"], dateCreated: "29 May 2015" },
+    { title: "title 1", tags: ["tag 1", "tag 2"], dateCreated: "29 May 2015" },
+    { title: "title 1", tags: ["tag 1", "tag 2"], dateCreated: "29 May 2015" },
+    { title: "title 1", tags: ["tag 1", "tag 2"], dateCreated: "29 May 2015" },
+    { title: "title 1", tags: ["tag 1", "tag 2"], dateCreated: "29 May 2015" },
+    { title: "title 1", tags: ["tag 1", "tag 2"], dateCreated: "29 May 2015" },
+    { title: "title 1", tags: ["tag 1", "tag 2"], dateCreated: "29 May 2015" },
+    { title: "title 1", tags: ["tag 1", "tag 2"], dateCreated: "29 May 2015" },
+    { title: "title 1", tags: ["tag 1", "tag 2"], dateCreated: "29 May 2015" },
   ];
   return (
     <>
       <DesktopPageHeader pageTitle={note} />
       <section className="align-page flex flex-col gap-1.5 px-4 py-5 lg:px-0 lg:py-0">
         <h1 className="text-preset-1 mb-4 lg:hidden">All Notes</h1>
-        {notes.length === 0 ? (
-          <EmptyNotes />
-        ) : (
-          notes.map((note, index) => (
-            <NoteComponent
-              key={index}
-              onSelectNote={() => setSelectedIndex(index)}
-              isSelected={selectedIndex === index}
-              noteTitle={note.title}
-              noteTags={note.tags}
-              dateCreated={note.dateCreated}
-            />
-          ))
-        )}
+        <div className="lg:grid grid-cols-[1fr_2fr_1fr]">
+          <div className="lg:w-72.5 lg:pl-8 lg:pr-4 lg:py-5 lg:border-r h-[calc(100vh-82px)] lg:border-neutral-200">
+            {notes.length === 0 ? (
+              <EmptyNotes />
+            ) : (
+              notes.map((note, index) => (
+                <NoteComponent
+                  key={index}
+                  onSelectNote={() => setSelectedIndex(index)}
+                  isSelected={selectedIndex === index}
+                  noteTitle={note.title}
+                  noteTags={note.tags}
+                  dateCreated={note.dateCreated}
+                />
+              ))
+            )}
+          </div>
+          <div className="lg:w-full lg:border-r h-[calc(100vh-82px)] lg:border-neutral-200"></div>
+          <div className="lg:w-72.5"></div>
+        </div>
       </section>
     </>
   );
