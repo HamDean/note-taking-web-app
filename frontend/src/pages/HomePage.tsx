67d3ad1fb16dc86ec4cc9c8d0cc5ee3799c2a96b
Hamdeen <hamdeen247@gmail.com>
2026-05-31 11:14:35 +0000

    Fix desktop notes overflow

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -12,13 +12,6 @@ const HomePage = () => {
     { title: "title 1", tags: ["tag 1", "tag 2"], dateCreated: "29 May 2015" },
     { title: "title 1", tags: ["tag 1", "tag 2"], dateCreated: "29 May 2015" },
     { title: "title 1", tags: ["tag 1", "tag 2"], dateCreated: "29 May 2015" },
-    { title: "title 1", tags: ["tag 1", "tag 2"], dateCreated: "29 May 2015" },
-    { title: "title 1", tags: ["tag 1", "tag 2"], dateCreated: "29 May 2015" },
-    { title: "title 1", tags: ["tag 1", "tag 2"], dateCreated: "29 May 2015" },
-    { title: "title 1", tags: ["tag 1", "tag 2"], dateCreated: "29 May 2015" },
-    { title: "title 1", tags: ["tag 1", "tag 2"], dateCreated: "29 May 2015" },
-    { title: "title 1", tags: ["tag 1", "tag 2"], dateCreated: "29 May 2015" },
-    { title: "title 1", tags: ["tag 1", "tag 2"], dateCreated: "29 May 2015" },
   ];
   return (
     <>
@@ -26,7 +19,7 @@ const HomePage = () => {
       <section className="align-page flex flex-col gap-1.5 px-4 py-5 lg:px-0 lg:py-0">
         <h1 className="text-preset-1 mb-4 lg:hidden">All Notes</h1>
         <div className="lg:grid grid-cols-[1fr_2fr_1fr]">
-          <div className="lg:w-72.5 lg:pl-8 lg:pr-4 lg:py-5 lg:border-r h-[calc(100vh-82px)] lg:border-neutral-200">
+          <div className="lg:w-72.5 lg:pl-8 lg:pr-4 lg:py-5 lg:overflow-y-auto lg:overflow-x-hidden lg:border-r h-[calc(100vh-82px)] lg:border-neutral-200">
             {notes.length === 0 ? (
               <EmptyNotes />
             ) : (
