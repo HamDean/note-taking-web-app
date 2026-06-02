5f3225fa2d760025de9e6355404b39b025bc2473
Hamdeen <hamdeen247@gmail.com>
2026-06-02 09:11:55 +0000

    Create add new note for mobile and table

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router";
 import Menubar from "./components/Menubar";
 import Navbar from "./components/Navbar";
 import SideBar from "./components/SideBar";
+import AddNoteMobile from "./components/AddNoteMobile";
 
 const Layout = () => {
   return (
@@ -9,6 +10,7 @@ const Layout = () => {
       <Navbar />
       <Menubar />
       <SideBar />
+      <AddNoteMobile />
       <main>
         <Outlet />
       </main>
