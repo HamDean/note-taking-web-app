e9eee6f48671e585d9a55112f0c73b814fef62ea
Hamdeen <hamdeen247@gmail.com>
2026-06-03 20:51:08 +0000

    Add Spring Security configuration and required dependencies

---
+++ backend/src/main/java/com/hamdeen/backend/configs/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.hamdeen.backend.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(c -> c
+                .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                .anyRequest().authenticated()
+            );
+
+        return http.build();
+    }
+}
