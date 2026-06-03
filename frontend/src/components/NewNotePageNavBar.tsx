ff0630e90d83af07a0084bbe0e762ad178461fbb
Hamdeen <hamdeen247@gmail.com>
2026-06-03 15:41:05 +0000

    Refactor NewNotePageNavBar: wrap back and cancel links in NavLink components for improved navigation

--- frontend/src/components/NewNotePageNavBar.tsx
+++ frontend/src/components/NewNotePageNavBar.tsx
@@ -1,15 +1,20 @@
+import { NavLink } from "react-router";
 import backIcon from "../assets/images/icon-arrow-left.svg";
 
 const NewNotePageNavBar = () => {
   return (
     <div className="h-8.5 border-b border-b-neutral-200 text-neutral-600 flex justify-between text-preset-5 lg:hidden">
-      <div className="flex items-center gap-1 cursor-pointer">
-        <img className="w-3 h-3" src={backIcon} alt="back" />
-        <span>Go Back</span>
-      </div>
+      <NavLink to={"/"}>
+        <div className="flex items-center gap-1 cursor-pointer">
+          <img className="w-3 h-3" src={backIcon} alt="back" />
+          <span>Go Back</span>
+        </div>
+      </NavLink>
 
       <div className="flex gap-4">
-        <span className="cursor-pointer">Cancel</span>
+        <NavLink to={"/"}>
+          <span className="cursor-pointer">Cancel</span>
+        </NavLink>
         <span className="text-blue-500 cursor-pointer">Save Note</span>
       </div>
     </div>
