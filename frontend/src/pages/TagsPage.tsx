61a7241775ec2753754f27f16ba1296bf3a2bbc8
Hamdeen <hamdeen247@gmail.com>
2026-06-03 10:38:10 +0000

    Hide tags page on desktop screens

--- frontend/src/pages/TagsPage.tsx
+++ frontend/src/pages/TagsPage.tsx
@@ -2,7 +2,7 @@ import TagMobile from "../components/TagMobile";
 
 const TagsPage = () => {
   return (
-    <div className="align-page p-4">
+    <div className="align-page p-4 lg:hidden">
       <h1 className="text-preset-1 mb-4">Tags</h1>
 
       <div className="flex flex-col gap-1">
