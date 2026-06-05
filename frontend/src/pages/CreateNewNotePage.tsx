1d2bd01922685ba696b9f4f81bea83948b0d9dd9
Hamdeen <hamdeen247@gmail.com>
2026-06-05 09:03:00 +0000

    Create the last edited row component

--- frontend/src/pages/CreateNewNotePage.tsx
+++ frontend/src/pages/CreateNewNotePage.tsx
@@ -1,11 +1,13 @@
 import NewNotePageNavBar from "../components/NewNotePageNavBar";
-import { useState } from "react";
-import tagIcon from '../assets/images/icon-tag.svg';
-import timeIcon from '../assets/images/icon-clock.svg'
+import { useEffect, useState } from "react";
+import tagIcon from "../assets/images/icon-tag.svg";
+import timeIcon from "../assets/images/icon-clock.svg";
 
 const CreateNewNotePage = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [tagInput, setTagInput] = useState("");
+  const [tags, setTags] = useState<string[]>([]);
 
   return (
     <section className="px-8 py-5">
@@ -13,16 +15,47 @@ const CreateNewNotePage = () => {
 
       <form action="" className="mt-4 flex flex-col gap-3">
         {/* title */}
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder="Enter a title..."
-            className="w-full text-preset-2 md:text-preset-1  text-gray-950 placeholder-neutral-950 bg-transparent outline-none"
-          />
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Enter a title..."
+          className="w-full text-preset-2 md:text-preset-1  text-gray-950 placeholder-neutral-950 bg-transparent outline-none"
+        />
 
         {/* content */}
-        
+        <div className="flex flex-col gap-3">
+          {/* tags row */}
+          <div className="flex gap-2 text-preset-6">
+            <div className="flex items-center gap-1.5 w-28.75">
+              <img className="w-4 h-4" src={tagIcon} alt="Tag" />
+              <span className="text-neutral-700">Tags</span>
+            </div>
+            <input
+              type="text"
+              name="tags"
+              value={tagInput}
+              onChange={(e) => setTagInput(e.target.value)}
+              placeholder={
+                tags.length === 0
+                  ? "Add tags separated by commas (e.g. Work, Planning)"
+                  : ""
+              }
+              className="text-neutral-950 placeholder-neutral-400 bg-transparent outline-none w-full placeholder:text-wrap"
+            />
+          </div>
+
+          {/* last edited row */}
+          <div className="flex gap-2 text-preset-6">
+            <div className="flex items-center gap-1.5 w-28.75">
+              <img className="w-4 h-4" src={timeIcon} alt="Last Edited" />
+              <span className="text-neutral-700">Last Edited</span>
+            </div>
+            <span className="text-preset-6 md:text-preset-5 text-neutral-400">
+              Not yet saved
+            </span>
+          </div>
+        </div>
       </form>
     </section>
   );
