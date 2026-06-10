d0d91b1c1cfcbe1572cc9cb7504892284e2a7fdc
Hamdeen <hamdeen247@gmail.com>
2026-06-10 22:14:52 +0000

    Build email and password input fields

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,7 +1,13 @@
 const LoginPage = () => {
   return (
     <div className="bg-neutral-100 dark:bg-neutral-700 h-screen px-4 md:px-30.75 flex flex-col justify-center">
-      <form className="bg-white dark:bg-neutral-950 px-4 py-10 rounded-12 border border-neutral-200 dark:border-neutral-800 md:min-w-130.5">
+      <form
+        className="bg-white dark:bg-neutral-950 px-4 py-10 rounded-12 
+            border border-neutral-200 dark:border-neutral-800 md:min-w-130.5
+            flex flex-col gap-4 md:max-w-130.5 lg:max-w-135
+            "
+      >
+        {/* Heading */}
         <div className="flex flex-col items-center gap-2">
           <Logo className="mb-2" />
           <h1 className="text-preset-1 text-neutral-950 dark:text-white">
@@ -10,6 +16,43 @@ const LoginPage = () => {
           <span className="text-preset-5 text-neutral-600 dark:text-neutral-300">
             Please log in to continue
           </span>
+        </div>
+
+        {/* Input Area */}
+        <div className="pt-6 flex flex-col gap-4">
+          {/* email section */}
+          <div className="flex flex-col gap-[6px]">
+            <label htmlFor="email" className="text-preset-4 text-neutral-950">
+              Email Address
+            </label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              placeholder="email@example.com"
+              className="placeholder:text-neutral-500 placeholder:text-preset-5 outline-none border
+                    border-neutral-300 rounded-8 h-10.5 px-4 py-3
+              "
+            />
+          </div>
+          {/* password section */}
+          <div className="flex flex-col gap-[6px]">
+            <div className="flex justify-between">
+              <label
+                htmlFor="password"
+                className="text-preset-4 text-neutral-950"
+              >
+                Password
+              </label>
+              <span className="text-neutral-600 underline text-xs cursor-pointer">Forgot</span>
+            </div>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className=" outline-none border border-neutral-300 rounded-8 h-10.5 px-4 py-3"
+            />
+          </div>
         </div>
       </form>
     </div>
