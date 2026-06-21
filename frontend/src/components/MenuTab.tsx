34d17f7de5238c5edf953a6973d137fda587f566
Hamdeen <hamdeen247@gmail.com>
2026-06-21 21:27:49 +0000

    Adjust Menutab width

--- frontend/src/components/MenuTab.tsx
+++ frontend/src/components/MenuTab.tsx
@@ -3,7 +3,7 @@ import MenuItem from "./MenuItem";
 
 const MenuTab = () => {
   return (
-    <nav className="flex items-center justify-between py-3 px-4 fixed bottom-0 border-t border-t-neutral-100">
+    <nav className="flex items-center w-full justify-between py-3 px-4 fixed bottom-0 border-t border-t-neutral-100">
       <MenuItem>
         <HomeIcon />
       </MenuItem>
