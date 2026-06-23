bff81cb4cd39ffe8f2cfe07af34bb14adb3cec27
Hamdeen <hamdeen247@gmail.com>
2026-06-23 10:24:56 +0000

    Make sidemenutab reusable

--- frontend/src/components/SideMenu.tsx
+++ frontend/src/components/SideMenu.tsx
@@ -1,16 +1,25 @@
 import logo from "../assets/images/logo.svg";
+import ArchivedIcon from "./icon-components/ArchivedIcon";
+import HomeIcon from "./icon-components/HomeIcon";
 import SideMenuTab from "./SideMenuTab";
 
 const SideMenu = () => {
   return (
-    <nav className="hidden lg:inline-flex flex-col px-4 py-3
-                    border-r border-r-neutral-200 h-screen gap-4">
+    <nav
+      className="hidden lg:inline-flex flex-col px-4 py-3
+                    border-r border-r-neutral-200 h-screen gap-4"
+    >
       <img className="h-7 w-23.75" src={logo} alt="Logo" />
 
       <div>
         {/* tabs */}
         <div>
-          <SideMenuTab />
+          <SideMenuTab name="All Notes">
+            <HomeIcon />
+          </SideMenuTab>
+          <SideMenuTab name="Archived Notes">
+            <ArchivedIcon />
+          </SideMenuTab>
         </div>
 
         {/* tags */}
--- frontend/src/components/SideMenuTab.tsx
+++ frontend/src/components/SideMenuTab.tsx
@@ -1,18 +1,25 @@
-import HomeIcon from "./icon-components/HomeIcon";
 import chevronRight from "../assets/images/icon-chevron-right.svg";
+import type { ReactNode } from "react";
 
-const SideMenuTab = () => {
+interface Props {
+  name: string;
+  isSelected?: boolean;
+  children: ReactNode;
+  onClick?: () => void;
+}
+
+const SideMenuTab = ({ name, children, isSelected = false }: Props) => {
   return (
     <div
       className="flex h-10 cursor-pointer items-center hover:bg-neutral-100
                     rounded-8 px-3 py-2.5 transition-colors"
     >
       <div className="flex gap-2 items-center min-w-60 text-preset-4 text-neutral-950">
-        <HomeIcon />
-        <span>All Notes</span>
+        {children}
+        <span>{name}</span>
       </div>
 
-      <img src={chevronRight} alt="Icon chevron right" />
+      {isSelected && <img src={chevronRight} alt="Icon chevron right" />}
     </div>
   );
 };
