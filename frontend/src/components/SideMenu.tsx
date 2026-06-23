6c579598cbf576c441bcd330ee28d530aa569035
Hamdeen <hamdeen247@gmail.com>
2026-06-23 10:58:06 +0000

    Apply darkmode colors

--- frontend/src/components/SideMenu.tsx
+++ frontend/src/components/SideMenu.tsx
@@ -9,7 +9,8 @@ const SideMenu = () => {
   return (
     <nav
       className="hidden lg:inline-flex flex-col px-4 py-3
-                    border-r border-r-neutral-200 h-screen gap-4"
+                    border-r border-r-neutral-200 h-screen 
+                    gap-4 dark:border-r-neutral-800"
     >
       <img className="h-7 w-23.75" src={logo} alt="Logo" />
 
--- frontend/src/components/SideMenuTab.tsx
+++ frontend/src/components/SideMenuTab.tsx
@@ -1,5 +1,5 @@
-import chevronRight from "../assets/images/icon-chevron-right.svg";
 import type { ReactNode } from "react";
+import ChevronRightIcon from "./icon-components/ChevronRightIcon";
 
 interface Props {
   name: string;
@@ -17,15 +17,16 @@ const SideMenuTab = ({
   return (
     <div
       onClick={() => onSelectTab(name)}
-      className={`flex h-10 cursor-pointer items-center hover:bg-neutral-100
-                    rounded-8 px-3 py-2.5 transition-colors ${isSelected && "bg-neutral-100"}`}
+      className={`flex h-10 cursor-pointer items-center hover:bg-neutral-100 hover:dark:bg-neutral-800
+                    rounded-8 px-3 py-2.5 transition-colors 
+                    ${isSelected && "bg-neutral-100 dark:bg-neutral-800"}`}
     >
       <div className="flex gap-2 items-center min-w-60 text-preset-4 text-neutral-950">
         {children}
-        <span>{name}</span>
+        <span className="dark:text-white">{name}</span>
       </div>
 
-      {isSelected && <img src={chevronRight} alt="Icon chevron right" />}
+      {isSelected && <ChevronRightIcon />}
     </div>
   );
 };
