724d9ac87c104ca488d585b04faf50d7de540269
Hamdeen <hamdeen247@gmail.com>
2026-06-30 18:58:12 +0000

    Build search input component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,17 +1,17 @@
 import settingsIcon from "../assets/images/icon-settings.svg";
+import SearchInput from "./SearchInput";
 
 const Header = () => {
   return (
-    <div className="h-20 w-full border-b border-b-neutral-200 hidden lg:flex
+    <div
+      className="h-20 w-full border-b border-b-neutral-200 hidden lg:flex
                     justify-between items-center py-5 px-8
-    ">
+    "
+    >
       <h1 className="text-neutral-950 text-preset-1">All Notes</h1>
 
-      <div className="flex">
-        <form action="">
-            <label htmlFor=""></label>
-            <input type="text" placeholder="search" />
-        </form>
+      <div className="flex gap-4">
+        <SearchInput />
         <img src={settingsIcon} alt="Settings" />
       </div>
     </div>
