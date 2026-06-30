3596f9b67011c8ff4930a6ccdb0202f3f398ea6a
Hamdeen <hamdeen247@gmail.com>
2026-06-30 18:48:14 +0000

    Set up header section of note layout

--- frontend/src/NotesLayout.tsx
+++ frontend/src/NotesLayout.tsx
@@ -1,6 +1,10 @@
+import Header from "./components/Header";
+
 const NotesLayout = () => {
   return (
-    <div>NotesLayout</div>
-  )
-}
-export default NotesLayout
+    <div className="w-full">
+      <Header />
+    </div>
+  );
+};
+export default NotesLayout;
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,4 @@
+const Header = () => {
+  return <div className="h-20 w-full border-b border-b-neutral-200 hidden lg:block">Header</div>;
+};
+export default Header;
