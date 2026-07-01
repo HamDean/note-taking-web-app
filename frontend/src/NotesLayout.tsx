dbb28be3662777c4f63189a08497e64f6260150c
Hamdeen <hamdeen247@gmail.com>
2026-07-01 21:04:02 +0000

    Receive note header dynamically.

--- frontend/src/NotesLayout.tsx
+++ frontend/src/NotesLayout.tsx
@@ -1,9 +1,11 @@
+import { useParams } from "react-router";
 import Header from "./components/Header";
 
 const NotesLayout = () => {
+  const { page } = useParams();
   return (
     <div className="w-full">
-      <Header />
+      <Header page={page} />
     </div>
   );
 };
